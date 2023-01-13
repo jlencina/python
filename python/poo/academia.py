b130178361497a743fa7889d263e5542e0731255
jlencina <juan.lencina@hotmail.com>
2023-01-13 12:51:55 -0300

    update studies python random chaos academy

--- python/poo/academia.py
+++ python/poo/academia.py
@@ -10,8 +10,69 @@ class Academia:
         self.porta_haleteres = {i: i for i in self.halteres}
         
     def listar_halteres(self):
-        return [i for i in self.porta_haleteres.values()]
-        
-self = Academia.reiniciar_o_dia()
+        return [i for i in self.porta_haleteres.values() if i != 0]
 
-print(self)
+    def listar_espacos(self):
+        return [i for i, j in self.porta_haleteres.items() if j == 0]
+        
+    def pegar_haltere(self, peso):
+        halt_pos = list(self.porta_haleteres.values()).index(peso)
+        key_halt = list(self.porta_haleteres.keys())[halt_pos]
+        self.porta_haleteres[key_halt] = 0
+        return peso
+
+    def devolver_halter(self, pos, peso):
+        self.porta_haleteres[pos] = peso
+
+    def calcular_caos(self):
+        num_caos = [i for i, j in self.porta_haleteres.items() if i != j ]
+        return len(num_caos) / len(self.porta_haleteres)
+
+class Usuario:
+    def __init__(self, tipo, academia):
+        self.tipo = tipo
+        self.academia = academia
+        self.peso = 0
+    
+    def iniciar_treino(self):
+        lista_pesos = self.academia.listar_halteres()
+        self.peso =  random.choice(lista_pesos)
+        self.academia.pegar_haltere(self.peso)
+
+    def finalizar_treino(self):
+        espacos = self.academia.listar_espacos()
+        
+        if self.tipo == 1:
+            if self.peso in espacos:
+                self.academia.devolver_halter(self.peso, self.peso)
+            else:
+                pos = random.choice(espacos)
+                self.academia.devolver_halter(pos, self.peso)
+
+        if self.tipo == 2:
+            pos = random.choice(espacos)
+            self.academia.devolver_halter(pos, self.peso)
+
+        self.peso == 0
+    
+academia = Academia()
+
+usuarios = [Usuario(1, academia) for i in range(10)]
+usuarios += [Usuario(2, academia) for i in range(1)]
+random.shuffle(usuarios)
+
+list_chaos = []
+
+for k in range(50):
+    academia.reiniciar_o_dia()
+    for i in range(10):        
+        random.shuffle(usuarios)
+        for user in usuarios:
+            user.iniciar_treino()
+        for user in usuarios:
+            user.finalizar_treino()
+    list_chaos += [academia.calcular_caos()]
+
+
+import seaborn as sns
+sns.displot(list_chaos)
